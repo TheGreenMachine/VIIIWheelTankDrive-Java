ed60be0d6a831a4e498661010f9277c68ae20437
Eric <congpeng89@gmail.com>
2014-11-18 18:12:48 -0600

    Added Gearshifter class.
    Shifts the gear to change the speed of the motor: full speed or half speed.

---
+++ src/com/edinarobotics/VIIITankDrive/subsystems/Gearshifter.java
@@ -0,0 +1,28 @@
+package com.edinarobotics.VIIITankDrive.subsystems;
+
+import com.edinarobotics.utils.subsystems.Subsystem1816;
+
+import edu.wpi.first.wpilibj.Relay;
+
+public class Gearshifter extends Subsystem1816 {
+    
+    private Relay gearshifter;
+    private Relay.Value value;
+    
+    public Gearshifter(int relay) {
+        gearshifter = new Relay(relay);
+        value = Relay.Value.kForward;
+    }
+
+    public void set(Relay.Value value) {
+        this.value = value;
+        update();
+    }
+    
+    public void update() {
+        
+    }
+    
+    
+    
+}
