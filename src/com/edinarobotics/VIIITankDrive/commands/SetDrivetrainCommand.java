cb7004b486173e4cdacc5fc6789f539365be4947
Eric <congpeng89@gmail.com>
2014-11-18 18:11:37 -0600

    Added SetDrivetrainCommand.
    Sets the Drivetrain to the respective parameters.

---
+++ src/com/edinarobotics/VIIITankDrive/commands/SetDrivetrainCommand.java
@@ -0,0 +1,40 @@
+package com.edinarobotics.VIIITankDrive.commands;
+
+import com.edinarobotics.VIIITankDrive.subsystems.Drivetrain;
+import com.edinarobotics.VIIIWheelTankDrive.Components;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class SetDrivetrainCommand extends Command {
+    
+    private Drivetrain drivetrain;
+    private double rightValue, leftValue;
+    
+    public SetDrivetrainCommand(double rightValue, double leftValue) {
+        super("SetDrivetrain");
+        drivetrain = Components.getInstance().drivetrain;
+        requires(drivetrain);
+        this.rightValue = rightValue;
+        this.leftValue = leftValue;
+    }
+
+    protected void initialize() {
+        drivetrain.setValues(rightValue, leftValue);
+    }
+
+    protected void execute() {
+        
+    }
+
+    protected boolean isFinished() {
+        return true;
+    }
+
+    protected void end() {
+        
+    }
+
+    protected void interrupted() {
+    
+    }
+    
+}
